2faef8c9657be9074da47a7e4c1582d57f6390da
AshutoshAvadhani <ava24ashutosh@gmail.com>
2025-04-26 17:48:30 -0400

    Added the init file for pytest module to run.

--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -6,12 +6,8 @@ from src.hello_world import PrintWorld
 def test_print():
     #pdb.set_trace()
     #raise Exception("Error") 
-    # result = PrintWorld()
-    # print("main() returned: {result}")
-    binary_path = os.path.join(os.environ["RUNFILES_DIR"], "src/hello_world")
-    result = subprocess.run(binary_path, capture_output=True, text=True)
-    print(f"DEBUG: Binary output -> {result.stdout.strip()}")  # Debugging
-    assert result.stdout.strip() == "Hello!"
+    result = PrintWorld()
+    assert result == "World Hello!"
 
 
 # def test_fail():
